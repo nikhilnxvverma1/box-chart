5115205a9a7fa378b7cb521565ed6fad04940731
Nikhil Verma <nikhilnxvverma1@aol.com>
2016-12-24 14:43:12 -0500

    Added sliding sidebar with swoosh animation

--- client/component/sidebar.component.ts
+++ client/component/sidebar.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { trigger,state,transition,style,animate } from '@angular/core';
 
 @Component({
-  selector: 'sidebar',
-  styleUrls:['../style/sidebar.component.scss'],
-  templateUrl: '../view/sidebar.component.html',
+    selector: 'sidebar',
+    styleUrls:['../style/sidebar.component.scss'],
+    templateUrl: '../view/sidebar.component.html',
+    animations: [
+    trigger('sidebarOpen', [
+      state('close', style({
+        left: '-300px'
+      })),
+      state('open', style({
+        left: '0px'
+      })),
+      transition('close => open', animate('100ms ease-in')),
+      transition('open => close', animate('100ms ease-out'))
+    ])
+  ]
 })
 export class SidebarComponent  {
-	
+    open=false;
 }
--- client/component/workspace.component.ts
+++ client/component/workspace.component.ts
@@ -1,14 +1,36 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild } from '@angular/core';
+import { animate,style,trigger,transition,state } from '@angular/core';
 import { TransformService,Point } from '../service/transform.service';
+import { SidebarComponent } from './sidebar.component';
 
 @Component({
     selector: 'workspace',
+    styleUrls:['../style/workspace.component.scss'],
     templateUrl: '../view/workspace.component.html',
+    animations:[
+        trigger('shiftMenuControls',[
+            state('unshifted',style({
+                left:'20px'
+            })),
+            state('shifted',style({
+                left:'300px'
+            })),
+            transition('unshifted => shifted', animate('100ms ease-in')),
+            transition('shifted => unshifted', animate('100ms ease-out'))
+        ])
+    ]
 })
 export class WorkspaceComponent  {
+
+    @ViewChild(SidebarComponent)
+    private sidebar:SidebarComponent;
 
     constructor(private transformService:TransformService){
       console.log("Workspace instantiated");
       console.log("p="+this.transformService.toModelSpace(new Point(43,54)));
+    }
+
+    toggleSidebar(){
+        this.sidebar.open=!this.sidebar.open;
     }
 }
