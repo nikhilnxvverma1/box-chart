d5af5d7aafe233f9f86c9f922b7eb77d6968fc6c
Nikhil Verma <nikhilnxvverma1@aol.com>
2017-01-02 11:06:51 +0530

    Modelled collection classes for objects. Also changed semantic model to hold classes in a tree and not a graph

--- client/model/object-model.ts
+++ client/model/object-model.ts
@@ -7,6 +7,7 @@ export interface Data{
 	stringRepresentation():string;
 }
 
+/** Holds a primitive value as a string */
 export class PrimitiveData implements Data{
 	value:string;
 	type:PrimitiveType;
@@ -24,7 +25,9 @@ export class PrimitiveData implements Data{
 	}
 }
 
+/** A struct that denotes an object which could either be a class object or an interface implementation */
 export abstract class ObjectData implements Data{
+	/** A 'ToString()' equivalent description of this object */
 	description:string;
 
 	abstract getTypeNode():TypeNode;
@@ -37,6 +40,7 @@ export abstract class ObjectData implements Data{
 	}
 }
 
+/** An implementation of an interface */
 export class InterfaceObjectData extends ObjectData{
 	interfaceDefinition:InterfaceDefinition;
 	
@@ -45,6 +49,7 @@ export class InterfaceObjectData extends ObjectData{
 	}
 }
 
+/** A concrete object holding several values for its fields */
 export class ClassObjectData extends ObjectData{
 
 	classDefinition:ClassDefinition;
@@ -56,11 +61,109 @@ export class ClassObjectData extends ObjectData{
 	}
 }
 
+/** Holds data for a variable. Weather primitive(through PrimitiveData) or a reference(through ObjectData or Collection) */
 export class DataHolder{
 	private variable:VariableDefinition;
 	private data:Data;
 
 	constructor(variable:VariableDefinition){
 		this.variable=variable;
+	}
+}
+
+/** Abstraction over different types of data collections  */
+export abstract class Collection implements Data{
+	/** Generic type that this collection is storing. */
+	generic:TypeNode;
+	/** Identifier name of this collection if it exists on its own. This can also be null. */
+	name:string;
+	/** Total nodes in this collection. (Does not mean capacity) */
+	count:number;
+
+	isPrimitive():boolean{
+		return false;
+	}
+
+	abstract stringRepresentation():string;
+}
+
+/** Data collection of Array types with a generic */
+export class ArrayData extends Collection{
+
+	data:Data[];
+
+	isPrimitive():boolean{
+		return false;
+	}
+
+	stringRepresentation():string{
+		return this.generic.getName()+"[]";
+	}
+}
+
+/** Data holder for the collection with a single link */
+export class SinglyLinkedNode{
+	next:SinglyLinkedNode;
+	data:Data;
+}
+
+/** Data holder for the collection with a two links */
+export class DoublyLinkedNode{
+	previous:DoublyLinkedNode;
+	next:DoublyLinkedNode;
+	data:Data;
+}
+
+/** Data holder for graph collection comprising of nodes and edges */
+export class GraphNode{
+	data:Data;
+	outgoingEdges:GraphEdge[];
+	incomingEdge:GraphEdge[];
+}
+
+/** Edge relationship of a graph node based collection represented by this class */
+export class GraphEdge{
+	from:GraphNode;
+	to:GraphNode;
+}
+
+/** Data collection of a doubly linked list */
+export class LinkedListData extends Collection{
+
+	start:DoublyLinkedNode;
+
+	stringRepresentation():string{
+		return "LinkedList<"+this.generic.getName()+">";
+	}
+}
+
+/** Data collection of a queue */
+export class QueueData extends Collection{
+
+	start:SinglyLinkedNode;
+	end:SinglyLinkedNode;
+	
+	stringRepresentation():string{
+		return "Queue<"+this.generic.getName()+">";
+	}
+}
+
+/** Data collection of a stack */
+export class StackData extends Collection{
+
+	top:SinglyLinkedNode;
+	
+	stringRepresentation():string{
+		return "Stack<"+this.generic.getName()+">";
+	}
+}
+
+/** Data collection of a graph */
+export class GraphData extends Collection{
+
+	node:GraphNode[];
+
+	stringRepresentation():string{
+		return "Graph<"+this.generic.getName()+">";
 	}
 }
--- client/model/semantic-model.ts
+++ client/model/semantic-model.ts
@@ -4,15 +4,8 @@ export class SemanticGraph{
 	typeNodeList:TypeNode[]=[];
 }
 
-export abstract class TypeNode{
-	incomingEdges:TypeEdge[]=[];
-	outgoingEdges:TypeEdge[]=[];
-	abstract getName():string;
-}
-
-export class TypeEdge{
-	from:TypeNode;
-	to:TypeNode;
+export interface TypeNode{
+	getName():string;
 }
 
 export enum PrimitiveType{//minding the name collisions
@@ -23,7 +16,7 @@ export enum PrimitiveType{//minding the name collisions
 	StringType
 }
 
-export class PrimitiveWrapper extends TypeNode{
+export class PrimitiveWrapper implements TypeNode{
 	type:PrimitiveType;
 
 	static getPrimtiveName(type:PrimitiveType):string{
@@ -80,24 +73,72 @@ export class VariableDefinition{
 	type:TypeNode;
 }
 
-export class ClassDefinition extends TypeNode{
+export class ClassDefinition implements TypeNode{
 	
 	name:string;
 	isAbstract:boolean;
 	fieldList:FieldMember[]=[];
 	methodList:MethodMember[]=[];
 
+	parentClass:ClassDefinition;
+	subClasses:ClassDefinition[];
+
 	getName():string{
 		return this.name;
 	}
 }
 
-export class InterfaceDefinition extends TypeNode{
+export class InterfaceDefinition implements TypeNode{
 
 	name:string;
 	methodList:MethodPrototype[]=[];//it can be assumed that all the methods are non static public
 
+	parentInterface:InterfaceDefinition;
+	subInterfaces:InterfaceDefinition;
+	implementingClasses:ClassDefinition;
+
 	getName():string{
 		return this.name;
+	}
+}
+
+export enum CollectionType{
+	Array=1,
+	LinkedList,
+	Stack,
+	Queue,
+	Graph
+}
+
+export class GenericCollection implements TypeNode{
+	type:CollectionType;
+	generic:TypeNode;
+
+	static getCollectionName(type:CollectionType,generic:TypeNode):string{
+		var genericTypeName="object"
+		if(generic!=null){
+			genericTypeName=generic.getName();
+		}
+
+		var collectionName:string;
+		switch(type){
+			case CollectionType.Array:
+				collectionName="Array";
+			case CollectionType.LinkedList:
+				collectionName="LinkedList";
+			case CollectionType.Stack:
+				collectionName="Stack";
+			case CollectionType.Queue:
+				collectionName="Queue";
+			case CollectionType.Graph:
+				collectionName="Graph";
+			default:
+				collectionName="Unknown Collection";
+		}
+		return collectionName+"<"+genericTypeName+">";
+	}
+
+	getName():string{
+		return GenericCollection.getCollectionName(this.type,this.generic);
 	}
 }
