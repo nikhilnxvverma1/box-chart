a0aa32a7db7bb19904e9bea9e940c3b04138aaec
Nikhil Verma <nikhilnxvverma1@aol.com>
2017-02-06 19:39:03 -0500

    Removed search box in dashboard plus formatting.

--- client/main.ts
+++ client/main.ts
@@ -2,13 +2,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
 import './global.scss';
-// import './assets/creation-drawer-icons/circle-generic-icon.svg'
-// require('./assets/creation-drawer-icons/circle-generic-icon.svg');
 
 declare let window:any;
 declare let Office:any;
 
-// Office.initialize = function () {
-// 	platformBrowserDynamic().bootstrapModule(AppModule);
-// };
-platformBrowserDynamic().bootstrapModule(AppModule);
+Office.initialize = function () {
+	platformBrowserDynamic().bootstrapModule(AppModule);
+};
+// platformBrowserDynamic().bootstrapModule(AppModule);
